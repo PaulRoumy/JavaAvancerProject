031d52de2168063980d04d07952140fdd9c652f4
Paul Roumy <paul.roumy@edu.itescia.fr>
2021-11-17 09:14:35 +0100

    Start project

---
+++ javaav/src/main/java/com/coding/javaav/UsersController.java
@@ -0,0 +1,27 @@
+package com.coding.javaav;
+
+import com.coding.javaav.dao.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsersController {
+
+    @Autowired
+    private UserDAO userService;
+
+    @RequestMapping("/users")
+    public String index(Model model){
+        model.addAttribute("listUsers", userService.listAll());
+        return "index";
+    }
+
+    @RequestMapping("/users/add")
+    public String add(Model model){
+        model.addAttribute("listUsers", userService.listAll());
+        return "index";
+    }
+
+}
