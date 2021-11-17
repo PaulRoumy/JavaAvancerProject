4956108b6462c8e7556c71bd969ce1c731bfa335
alban <lacou.alban@gmail.com>
2021-11-17 14:29:15 +0100

    test

--- javaav/src/main/java/com/coding/javaav/ProductsController.java
+++ javaav/src/main/java/com/coding/javaav/ProductsController.java
@@ -1,4 +1,20 @@
 package com.coding.javaav;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class ProductsController {
+
+    @RequestMapping("/products")
+    public String pagination(@RequestParam(value= "range") String range){
+        System.out.println(range);
+
+
+
+        return "pagination";
+    }
 }
