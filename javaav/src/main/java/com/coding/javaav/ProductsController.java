1d42be605a8565813e941aea2d5bec54018eccd3
Paul Roumy <paul.roumy@edu.itescia.fr>
2021-11-17 16:03:24 +0100

    Products Crud finish + RessourceExeption add

--- javaav/src/main/java/com/coding/javaav/ProductsController.java
+++ javaav/src/main/java/com/coding/javaav/ProductsController.java
@@ -3,7 +3,9 @@ package com.coding.javaav;
 
 import com.coding.javaav.dao.ProductsDAO;
 import com.coding.javaav.models.Products;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -36,6 +38,27 @@ public class ProductsController {
     @RequestMapping(value ="/products/supp/{id}",method= RequestMethod.DELETE)
     public String supp(@PathVariable int id){
         productsService.suppProducts(id);
+        return "index";
+    }
+
+    @RequestMapping(value = "/products/getById/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Products> getById(@PathVariable long id, Model model){
+        Products p = productsService.findById(id);
+        if(p == null) {
+            throw  new ResourceNotFoundException("User not found on :: " + id);
+        }
+        return ResponseEntity.ok().body(p);
+    }
+
+    @RequestMapping(value = "/products/modify/{id}", method = RequestMethod.PATCH)
+    public String modify(@PathVariable int id, @RequestBody Products input){
+        Products p = new Products();
+        p.setCategoryId(input.getCategoryId());
+        p.setRating(input.getRating());
+        p.setname(input.getname());
+        p.setType(input.getType());
+        productsService.modifyProducts(id, p);
+
         return "index";
     }
 }
