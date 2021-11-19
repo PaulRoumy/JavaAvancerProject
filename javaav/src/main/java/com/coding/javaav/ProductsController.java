7ddbc1fc035926496019cc813a78f1255f36b4d1
alban <lacou.alban@gmail.com>
2021-11-19 09:36:58 +0100

    refactorisation du code

--- javaav/src/main/java/com/coding/javaav/ProductsController.java
+++ javaav/src/main/java/com/coding/javaav/ProductsController.java
@@ -52,23 +52,8 @@ public class ProductsController {
 
     @RequestMapping("/products")
     public ResponseEntity<List<Products>> pagination(@RequestParam(value="range") String range){
-        //"1-1"
-        String separateur = "-";
-        String[] value = range.split(separateur);
-        String sql = "SELECT * FROM Products LIMIT ? OFFSET ?";
-        int limit = Integer.parseInt(value[1]);
-        int offset = Integer.parseInt(value[0]);
+        return productsService.pagination(range);
 
-        List<Products> list = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Products.class), limit, offset);
-
-        int test = limit+offset;
-        String retour = String.valueOf(limit)+"/"+String.valueOf(test);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Range:", retour);
-        headers.add("Accept-Range:", "product: "+limit);
-
-        return new ResponseEntity<>(list, headers, HttpStatus.OK);
     }
 
 
@@ -85,47 +70,7 @@ public class ProductsController {
             @RequestParam(value="type",required = false) String type,
             @RequestParam(value="rating",required = false) String rating
             ){
-        String sql = "";
-
-        ArrayList value = new ArrayList() ;
-        ArrayList condition = new ArrayList() ;
-
-        if(name != null){
-            condition.add("name = ?");
-            value.add(name);
-        }
-        if (type != null){
-            condition.add("type = ?");
-            value.add(type);
-        }
-        if (rating != null){
-            condition.add("rating = ?;");
-            value.add(rating);
-        }
-
-
-
-        List<Products> list = new ArrayList<>();
-        int valueSize = value.size();
-        switch (valueSize){
-            case 1:
-                sql = "SELECT * FROM Products WHERE "+condition.get(0);
-                list = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Products.class), value.get(0));
-                break;
-            case 2:
-                sql = "SELECT * FROM Products WHERE "+condition.get(0)+" AND "+condition.get(1);
-                list = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Products.class),value.get(0),value.get(1));
-                break;
-            case 3:
-                sql = "SELECT * FROM Products WHERE "+condition.get(0)+" AND "+condition.get(1)+" AND "+condition.get(2);
-                list = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Products.class),value.get(0),value.get(1),value.get(2));
-                break;
-        }
-
-
-
-
-        return list;
+        return productsService.recherche(name,type,rating);
     }
 
 
--- javaav/src/main/java/com/coding/javaav/dao/ProductsDAO.java
+++ javaav/src/main/java/com/coding/javaav/dao/ProductsDAO.java
@@ -6,12 +6,18 @@ import com.coding.javaav.models.Category;
 import com.coding.javaav.models.Products;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 @Repository
 public class ProductsDAO {
     @Autowired
@@ -74,9 +80,69 @@ public class ProductsDAO {
         String sql = "SELECT * FROM Products WHERE id=? ;";
         return jdbcTemplate.queryForObject(sql, BeanPropertyRowMapper.newInstance(Products.class), id);
     }
-
     public int modifyProducts(int id, Products p){
         String sql="UPDATE Products SET type=?, name=?, rating=?, categoryId=? WHERE id=?";
         return jdbcTemplate.update(sql, p.getType(), p.getname(), p.getRating(),p.getCategoryId(),id);
+    }
+    public ResponseEntity<List<Products>> pagination(String range){
+        String separateur = "-";
+        String[] value = range.split(separateur);
+        String sql = "SELECT * FROM Products LIMIT ? OFFSET ?";
+        int limit = Integer.parseInt(value[1]);
+        int offset = Integer.parseInt(value[0]);
+
+        List<Products> list = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Products.class), limit, offset);
+
+        int test = limit+offset;
+        String retour = String.valueOf(limit)+"/"+String.valueOf(test);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Range:", retour);
+        headers.add("Accept-Range:", "product: "+limit);
+
+        return new ResponseEntity<>(list, headers, HttpStatus.OK);
+    }
+    public List<Products> recherche(String name,String type,String rating){
+        String sql = "";
+
+        ArrayList value = new ArrayList() ;
+        ArrayList condition = new ArrayList() ;
+
+        if(name != null){
+            condition.add("name = ?");
+            value.add(name);
+        }
+        if (type != null){
+            condition.add("type = ?");
+            value.add(type);
+        }
+        if (rating != null){
+            condition.add("rating = ?;");
+            value.add(rating);
+        }
+
+
+
+        List<Products> list = new ArrayList<>();
+        int valueSize = value.size();
+        switch (valueSize){
+            case 1:
+                sql = "SELECT * FROM Products WHERE "+condition.get(0);
+                list = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Products.class), value.get(0));
+                break;
+            case 2:
+                sql = "SELECT * FROM Products WHERE "+condition.get(0)+" AND "+condition.get(1);
+                list = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Products.class),value.get(0),value.get(1));
+                break;
+            case 3:
+                sql = "SELECT * FROM Products WHERE "+condition.get(0)+" AND "+condition.get(1)+" AND "+condition.get(2);
+                list = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Products.class),value.get(0),value.get(1),value.get(2));
+                break;
+        }
+
+
+
+
+        return list;
     }
 }
